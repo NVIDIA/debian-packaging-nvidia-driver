580f06b40e30560bdf0e8acd8ce706d8dc68f802
Andreas Beckmann <debian@abeckmann.de>
2012-09-12 10:17:11 +0000

    bump conftest.sh sync versions, no changes
    git-svn-id: svn://svn.debian.org/svn/pkg-nvidia/packages/nvidia-graphics-drivers/trunk@3378 b6a4b1ba-b714-0410-8a2d-fc0b36c6496e

--- debian/module/conftest.h
+++ debian/module/conftest.h
@@ -1,4 +1,4 @@
-/* synchronized with conftest.sh from 304.43, 295.75, 173.14.35, 96.43.20, 71.86.15 */
+/* synchronized with conftest.sh from 304.48, 295.75, 173.14.35, 96.43.23, 71.86.15 */
 
 #ifndef LINUX_VERSION_CODE
 #include <linux/version.h>
